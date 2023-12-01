fba0a0059965d20880c5613b7b69a2a01da147e1
Muhammad Ahmad <69284724+Ahmad1786@users.noreply.github.com>
2023-12-01 18:42:57 -0500

    add a similar general api for new databases

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -32,6 +32,33 @@ class Catalog(Base):
     def __repr__(self):
         return f'<Catalog {self.subj} {self.crse}>'
     
+class Infrastructure(Base):
+    __tablename__ = 'locations'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    description = Column(Text)
+    keywords = Column(String(255))
+    categories = Column(String(255))
+    latitude = Column(Float)
+    longitude = Column(Float)
+    departments = Column(String(255))
+    
+    def __repr__(self):
+        return f'<Location {self.id}:  {self.name}>'
 
+class CurrentEvents(Base):
+    __tablename__ = 'events'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    description = Column(Text)
+    host = Column(String(255))
+    location = Column(String(255))
+    start = Column(DateTime)
+    end = Column(DateTime)
+    last_modified = Column(DateTime)
+    url = Column(String(255))
+    
+    def __repr__(self):
+        return f'<Event {self.id}:  {self.name}>'
